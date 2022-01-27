16bcfbe0d9f0f35628649525191c31e3f5faa373
ms <altjsvnf@naver.com>
2022-01-27 20:45:44 +0900

    승자 확인 구현

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ let com_score = document.querySelector('.score_com')
 
 //img
 let img = document.querySelectorAll('.rps>img');
-let img_list = [rock, scissors, paper];
 
 //리셋버튼 구현
 
@@ -36,6 +35,7 @@ const random_img = () => {
         view_com.removeChild(view_com.children[1])
     }
     com_img.setAttribute("src", img[ranNum].getAttribute("src"));
+    com_img.setAttribute("name", img[ranNum].getAttribute("alt"));
     com_img.className = "curCom";
     view_com.appendChild(com_img);
 }
@@ -48,13 +48,38 @@ const my_select = (e) => {
             view_me.removeChild(view_me.children[1])
         }
         my_img.setAttribute("src", e.target.getAttribute("src"));
+        my_img.setAttribute("name", e.target.getAttribute("alt"));
         my_img.className = "curMe";
         view_me.appendChild(my_img);
         setTimeout(() => {
             random_img();
+            whoWin();
         }, 500);
 }
 
+//승자 확인
+const whoWin = () => {
+    const curMe = document.querySelector(".curMe");
+    const curCom = document.querySelector(".curCom");
+    if (
+        (curMe.name == '주먹' && curCom.name == '가위') ||
+        (curMe.name == '가위' && curCom.name == '보') ||
+        (curMe.name == '보' && curCom.name == '주먹')
+        )    
+    {
+        my_score.innerText = parseInt(my_score.innerText) + 1;
+    }
+    if (
+        (curCom.name == '주먹' && curMe.name == '가위') ||
+        (curCom.name == '가위' && curMe.name == '보') ||
+        (curCom.name == '보' && curMe.name == '주먹')
+        )    
+    {
+        com_score.innerText = parseInt(com_score.innerText) + 1;
+    }
+}
+
+//버튼 누르기
 rock.addEventListener('click', my_select);
 scissors.addEventListener('click', my_select);
 paper.addEventListener('click', my_select);
